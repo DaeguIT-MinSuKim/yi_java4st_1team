d4111330bb561690bfe647706a21bc95fe2394cf
jisu25 <kimpigu.zz@gmail.com>
2020-08-25 17:11:28 +0900

    frame resizing

--- src/hairrang/HairshopManagementProgram.java
+++ src/hairrang/HairshopManagementProgram.java
@@ -27,19 +27,25 @@ import hairrang.table.hairOrderSearch;
 import hairrang.table.hairmanagement;
 
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
 public class HairshopManagementProgram extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private JPanel layeredPane;
 	private JPanel panel_1;
-	private hairmanagement p3;
-	private GuestSearch p2;
-	private GuestManagement p1;
+	
+	private JPanel p1;
+	private JPanel p2;
+	private JPanel p3;
+	private JPanel p4;
+	private JPanel p5;
+	private JPanel emptyPane;
+	
 	private JPanel menuPanel;
 	
-	private Dimension btnMenuDim = new Dimension(120, 60);
-	private Dimension btnHomeDim = new Dimension(230, 100);
+	private Dimension btnMenuDim = new Dimension(100, 60);
+	private Dimension btnHomeDim = new Dimension(230, 90);
 	
 	private JPanel menuBtnsPanel;
 	private JButton btnMenu1;
@@ -47,16 +53,13 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 	private JButton btnMenu3;
 	private JButton btnMenu4;
 	private JButton btnMenu5;
-	private JButton btnMenu6;
 	private JButton btnHome;
 	private JButton[] btnsMenu;
 
 	private Color mainColor = new Color(153, 102, 255);
-	private hairOrderSearch p4;
-	private ReportPanel p5;
-	private JPanel p6;
-	private JPanel emptyPane;
 	private JPanel[] pArr;
+	private JLabel lblMenuName;
+	private JPanel sidePanel;
 
 	/**
 	 * Launch the application.
@@ -75,7 +78,7 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 
 	private void initComponents() throws FontFormatException, IOException {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 50, 1280, 960);
+		setBounds(100, 50, 1024, 768);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -88,36 +91,34 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 		/* panel 정의 */
 
 		layeredPane = new JPanel();
-		//      panel.setBounds(30, 200, 460, 690);
-		layeredPane.setBounds(30, 200, 970, 690);
+		layeredPane.setBounds(30, 160, 700, 540);
 		contentPane.add(layeredPane);
 		layeredPane.setLayout(new CardLayout(0, 0));
 
-		p1 = new GuestManagement();
+		
+		// dto 수정하면서 각 패널들 고장나서 기본 JPanel로 바꿔 넣음
+		// dto에 맞게 구현하고 나면 morph 하세요.
+		
+		p1 = new JPanel();
 		layeredPane.add(p1, "name_190917990402500");
 		p1.setLayout(null);
 
-		p2 = new GuestSearch();
+		p2 = new JPanel();
 		layeredPane.add(p2, "name_191035038994000");
 		p2.setLayout(null);
 
-		p3 = new hairmanagement();
+		p3 = new JPanel();
 		layeredPane.add(p3, "name_191036958001300");
 		p3.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		p4 = new hairOrderSearch();
+		p4 = new JPanel();
 		layeredPane.add(p4, "name_779538988255300");
 		p4.setLayout(new BorderLayout(0, 0));
 		
-		p5 = new ReportPanel();
+		p5 = new JPanel();
 		layeredPane.add(p5, "name_779540758050400");
 		
-//		p6 = new chartPanel();
-		p6 = new JPanel();
-		layeredPane.add(p6, "name_779542901747700");
-		p6.setLayout(new GridLayout(1, 0, 0, 0));
-
-		pArr = new JPanel[] {	p1, p2, p3, p4, p5, p6 };
+		pArr = new JPanel[] { p1, p2, p3, p4, p5};
 		
 		/* 메뉴부 */
 		
@@ -126,12 +127,13 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 		File gfont_file = new File(fontPath + "GmarketSansTTFBold.ttf");
 		Font gSansBold = Font.createFont(Font.TRUETYPE_FONT, gfont_file);
 		Font gSansBold28 = gSansBold.deriveFont(28f);
-		Font gSansBold20 = gSansBold.deriveFont(16f);
+		Font gSansBold20 = gSansBold.deriveFont(20f);
+		Font gSansBold16 = gSansBold.deriveFont(16f);
 
 		
 		menuPanel = new JPanel();
 		menuPanel.setBackground(mainColor);
-		menuPanel.setBounds(0, 0, 1264, 100);
+		menuPanel.setBounds(0, 0, 1008, 90);
 		contentPane.add(menuPanel);
 		menuPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
@@ -139,7 +141,7 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 
 		btnHome = new JButton("HAIRRANG");
 		btnHome.setFont(gSansBold28);
-		btnHome.setPreferredSize(new Dimension(230, 100));
+		btnHome.setPreferredSize(new Dimension(230, 90));
 		btnHome.addActionListener(this);
 		btnHome.setOpaque(false);
 		btnHome.setContentAreaFilled(false);
@@ -147,29 +149,25 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 		btnHome.setForeground(Color.white);
 		menuPanel.add(btnHome);
 
-		
 		emptyPane = new JPanel();
-		emptyPane.setPreferredSize(new Dimension(40, 100));
+		emptyPane.setPreferredSize(new Dimension(20, 90));
 		emptyPane.setOpaque(false);
 		menuPanel.add(emptyPane);
-		
-		
 		
 		menuBtnsPanel = new JPanel();
 		menuBtnsPanel.setOpaque(false);
 		menuPanel.add(menuBtnsPanel);
 		menuBtnsPanel.setLayout(new GridLayout(1, 6, 0, 0));
-		menuBtnsPanel.setPreferredSize(new Dimension(700, 100));
+		menuBtnsPanel.setPreferredSize(new Dimension(700, 90));
 
 		btnMenu1 = new JButton("고객 관리");
 		btnMenu2 = new JButton("고객 검색");
 		btnMenu3 = new JButton("주문");
 		btnMenu4 = new JButton("주문 내역");
-		btnMenu5 = new JButton("보고서");
-		btnMenu6 = new JButton("차트");
+		btnMenu5 = new JButton("통계");
 
 		
-		btnsMenu = new JButton[] { btnMenu1, btnMenu2, btnMenu3, btnMenu4, btnMenu5, btnMenu6 };
+		btnsMenu = new JButton[] { btnMenu1, btnMenu2, btnMenu3, btnMenu4, btnMenu5};
 
 		for (int i = 0; i < btnsMenu.length; i++) {
 			btnsMenu[i].addActionListener(this);
@@ -177,7 +175,7 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 			btnsMenu[i].setOpaque(false);
 			btnsMenu[i].setContentAreaFilled(false);
 			btnsMenu[i].setBorderPainted(false);
-			btnsMenu[i].setFont(gSansBold20);
+			btnsMenu[i].setFont(gSansBold16);
 			btnsMenu[i].setForeground(Color.white);
 		}
 		
@@ -186,19 +184,29 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 		menuBtnsPanel.add(btnMenu3);
 		menuBtnsPanel.add(btnMenu4);
 		menuBtnsPanel.add(btnMenu5);
-		menuBtnsPanel.add(btnMenu6);
 
+		
+		lblMenuName = new JLabel("▶ 고객 검색");
+		lblMenuName.setBounds(30, 120, 140, 32);
+		lblMenuName.setFont(gSansBold20);
+		contentPane.add(lblMenuName);
+		
+		sidePanel = new JPanel();
+		sidePanel.setBounds(750, 90, 258, 639);
+		contentPane.add(sidePanel);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		/*
 		if (e.getSource() == btnMenu6) {
-			JFrame temp = new JFrame();
+			JFrame temp = new JFrame();.
 			temp.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			temp.setBounds(100, 100, 940, 690);
 			temp.getContentPane().add(new chartPanel());
 			temp.setVisible(true);
 			return;
 		}
+		*/
 		
 //		if (e.getSource() == btnMenu6 ) {
 //			p6.add(new chartPanel());
@@ -206,7 +214,7 @@ public class HairshopManagementProgram extends JFrame implements ActionListener 
 //			return;
 //		}
 		
-		if (e.getSource() == btnMenu1 || e.getSource() == btnMenu2 || e.getSource() == btnMenu3 || e.getSource() == btnMenu4 || e.getSource() == btnMenu5 ||  e.getSource() == btnMenu6) {
+		if (e.getSource() == btnMenu1 || e.getSource() == btnMenu2 || e.getSource() == btnMenu3 || e.getSource() == btnMenu4 || e.getSource() == btnMenu5 ) {
 			btnsMenuActionPerformed(e);
 		}
 	}
